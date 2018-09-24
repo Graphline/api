0b78a8a8063878377b038f26c8c5aa4242501bc9
DecentM <decentm@decentm.com>
2018-09-25 00:18:12 +0200

    chore: Add missing dependencies

---
+++ src/express/backend.js
@@ -0,0 +1,61 @@
+import schema from '~/graphql'
+import MatomoApi from 'matomo-reporting-js'
+import fetch from 'node-fetch'
+import raven from 'raven'
+import {Agent,} from 'https'
+
+// import pify from 'pify'
+// import staticConfig from 'lib/config'
+// import jwt from 'jsonwebtoken'
+
+const agent = new Agent({
+  'rejectUnauthorized': false,
+})
+const matomo = new MatomoApi({
+  fetch,
+  agent,
+  'handler': (error) => {
+    raven.captureException(error)
+    /* eslint-disable-next-line no-console */
+    console.error(error)
+  },
+}, {
+  'endpoint': 'https://pwk.decentm.com/index.php',
+  'idSite':   10,
+})
+
+// const getViewer = async (token, {User,}) => {
+//   const config = await staticConfig()
+//   let viewer = null
+//
+//   if (!token) {
+//     return null
+//   }
+//
+//   try {
+//     const decoded = await pify(jwt.verify)(token, config.get('keypair.clientprivate'))
+//
+//     viewer = User.findById(decoded.userId)
+//   } catch (error) {
+//     return null
+//   }
+//
+//   return viewer
+// }
+
+export default async (req, res, next) => {
+  const token = req.headers.authorization
+  // const viewer = await getViewer(token, model)
+
+  return {
+    'tracing': process.env.NODE_ENV === 'development',
+    'context': {
+      token,
+      matomo,
+      // viewer,
+      req,
+      // ...model,
+    },
+    schema,
+  }
+}
--- src/express/routes.js
+++ src/express/routes.js
@@ -1,7 +1,15 @@
+import {graphqlExpress,} from 'apollo-server-express'
+import bodyParser from 'body-parser'
 import playground from 'graphql-playground-middleware-express'
+
+import backend from './backend'
 
 export default async ({app,}) => {
   app.get('/playground', playground({
     'endpoint': '/',
   }))
+
+  app.post('/', bodyParser.json({
+    'limit': 120000,
+  }), graphqlExpress(backend))
 }
